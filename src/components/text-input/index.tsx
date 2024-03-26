fd2be608c6742a703de1a49da04e1ae6d1ec5332
Chamara-Wijepala <mr.worf777@gmail.com>
2024-03-26 19:04:41 +0530

    Create text input component

---
+++ src/components/text-input/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+
+function TextInput({
+	state,
+	setState,
+	isStateValid,
+	label,
+	note,
+}: {
+	state: string;
+	setState: React.Dispatch<React.SetStateAction<string>>;
+	isStateValid: boolean;
+	label: string;
+	note: string;
+}) {
+	const [isInputFocused, setIsInputFocused] = useState(false);
+	const [showValidationStatus, setShowValidationStatus] = useState(false);
+
+	return (
+		<div className="relative">
+			<label htmlFor={label} className="text-slate-700 text-sm">
+				{label}*
+			</label>
+			<div className="flex gap-2 items-center border-b-2 border-slate-250">
+				<input
+					type="text"
+					value={state}
+					id={label}
+					autoComplete="off"
+					required
+					aria-invalid={isStateValid ? 'false' : 'true'}
+					aria-describedby="note"
+					onChange={(e) => setState(e.target.value)}
+					onFocus={() => setIsInputFocused(true)}
+					onBlur={() => {
+						setIsInputFocused(false);
+						setShowValidationStatus(true);
+					}}
+					className="py-2 outline-none md:text-xl grow bg-transparent"
+				/>
+
+				{showValidationStatus &&
+					(isStateValid ? (
+						<AiOutlineCheckCircle className="md:min-h-6 md:min-w-6 text-emerald-500" />
+					) : (
+						<AiOutlineCloseCircle className="md:min-h-6 md:min-w-6 text-rose-500" />
+					))}
+			</div>
+
+			<div
+				className={classNames(
+					isInputFocused ? 'block' : 'sr-only',
+					'bg-white p-2 flex gap-2 rounded-md shadow-md mt-4 z-50'
+				)}
+			>
+				<AiOutlineExclamationCircle className="m-1 md:min-h-5 md:min-w-5" />
+				<p id="note">{note}</p>
+			</div>
+		</div>
+	);
+}
+
+export default TextInput;
